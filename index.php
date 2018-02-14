52b5c0b0f68a8e8033bd3052ba83df857e89ee08
Tyler Roemer <troemer@mail.greenriver.edu>
2018-02-14 14:02:20 -0500

    created index.php and defined default route

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    //php error reporting
+    ini_set("display_errors", 1);
+    error_reporting(E_ALL);
+
+    //require the autoload file
+    require_once ('vendor/autoload.php');
+
+    //create an instance of the base class
+    $f3 = Base::instance();
+
+    //set debug level
+    //0 = no error reporting, 3 = report all errors
+    $f3->set('DEBUG', 3);
+
+    //Define a default route
+    $f3->route('GET /', function() {
+        echo '<h1>My Pets</h1>';
+        echo "<a href='./order'>Order a Pet</a>";
+    });
+
+    $f3->route('GET /order', function() {
+
+    });
+
+    //Run Fat-Free
+    $f3->run();
